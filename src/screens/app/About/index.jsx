b2f648929c7230f612b91cefaee28bb986b09afb
Sandip <sandipganava1234@gmail.com>
2024-05-21 10:23:46 +0530

    Update login page styles

--- src/screens/app/About/index.jsx
+++ src/screens/app/About/index.jsx
@@ -1,105 +1,327 @@
-import { Dimensions, ScrollView, Image, TouchableOpacity, StyleSheet, View, Text } from 'react-native';
-import React from "react";
-import Animated, { interpolate, useAnimatedRef, useAnimatedStyle, useScrollViewOffset } from 'react-native-reanimated';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useNavigation } from '@react-navigation/native';
+import moment from 'moment';
+import React, { useEffect, useState } from 'react';
+import { Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/dist/MaterialCommunityIcons';
+import AgeCount from '../component/AgeCount';
+import CustomModal from '../component/CustomModal';
 
-const { width } = Dimensions.get('window')
-const IMG_HEIGHT = 300
+const FamilyDetailsPage = ({navigation}) => {
+ 
 
-const Aboutus = () => {
 
-    const scrollRef = useAnimatedRef();
-    const scrolloffset = useScrollViewOffset(scrollRef);
-
-    const imageAnimatedStyle = useAnimatedStyle(() => {
-        return {
-            transform: [
-                {
-                    translateY: interpolate(
-                        scrolloffset.value,
-                        [-IMG_HEIGHT, 0, IMG_HEIGHT],
-                        [-IMG_HEIGHT / 2, 0, IMG_HEIGHT * 0.75]
-                    ),
-
-                },
-                {
-                    scale: interpolate(
-                        scrolloffset.value,
-                        [-IMG_HEIGHT, 0, IMG_HEIGHT],
-                        [2, 1, 1]
-                    )
-                }
-            ]
-        }
-    })
+  const renderParentDetails = () => {
+    if (!parentsData) {
+      return null;
+    }
+    const formattedDate = moment(parentsData?.dob).format('DD/MM/YYYY');
+    const parentsAge = AgeCount(parentsData?.dob);
 
     return (
-        <View style={styles.container}>
+      <>
+        <View style={[styles.MainContainer, { marginBottom: 30 }]}>
+          <View style={styles.familyItem}>
+            <Pressable
+              style={styles.EditIcon}
+             >
+              <MaterialCommunityIcons
+                name="account-edit"
+                size={30}
+                color="#00000090"
+              />
+            </Pressable>
 
-            <Animated.ScrollView ref={scrollRef} scrollEventThrottle={16}>
-                <View>
-                    <Animated.Image source={{ uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGsmGhSaJcQOzDWEwYB31PkUQZTsCsW4YZmQYh6B2c7Q&s' }}
-                        style={[styles.image, imageAnimatedStyle]} />
+            <View style={styles.dropdownContent}>
+              <View>
+                <Text style={styles.FamilyName}>
+                 asasas asasas
+                </Text>
+              </View>
+            </View>
 
-                </View>
+            <View style={styles.row}>
+              <Text style={styles.familylabel}>{t('name')} : </Text>
+              <Text style={styles.familyDetails}>
+                {parentsData?.lastname} {parentsData?.firstname} {parentsData?.middlename}{' '}
 
-                <View className="bg-white">
+              </Text>
+            </View>
 
-                    <View className="ml-[10px]">
-                        <Text className="tracking-wider text-neutral-700 font-semibold text-3xl">About Us.</Text>
-                        <View className="w-12 h-2 bg-red-400 my-2 rounded"></View>
-                        <Text className="text-[17px] font-medium mt-[10px]">
-                            Were on a missionto add relevancy to every online experince
-                        </Text>
-                    </View>
+            <View style={styles.row}>
+              <Text style={styles.familylabel}>{t('personalid')} : </Text>
+              <Text style={styles.familyDetails}>
+                {parentsData?.personal_id}
+              </Text>
+            </View>
 
+            <View style={styles.row}>
+              <Text style={styles.familylabel}>{t('dateofbirth')} : </Text>
+              <Text style={styles.familyDetails}>{formattedDate}</Text>
+            </View>
 
-                    <TouchableOpacity className="flex items-center justify-center bg-rose-400 h-10 w-40 ml-3 mt-4 mb-10 rounded">
-                        <Text className="tracking-wider text-neutral-700 font-medium text-base">Learn More</Text>
-                    </TouchableOpacity>
+            <View style={styles.row}>
+              <Text style={styles.familylabel}>{t('age')} : </Text>
+              <Text style={styles.familyDetails}>{parentsAge} years</Text>
+            </View>
 
-                    <View className="p-[10px]">
-                        <Text className="tracking-wider text-neutral-700 font-semibold text-3xl">Our Purpose</Text>
-                        <View className="w-12 h-2 bg-red-400 my-2 rounded"></View>
-                        <Text className="tracking-wider text-neutral-700 font-medium text-base">
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur repellat, iste iusto, culpa recusandae fugiat deserunt molestias praesentium nulla magnam tenetur! Sit neque sapiente tempore, laudantium perferendis eius tenetur dicta.
-                        </Text>
-                    </View>
+            <View style={styles.row}>
+              <Text style={styles.familylabel}>{t('gender')} :</Text>
+              <Text style={styles.familyDetails}>{parentsData?.gender}</Text>
+            </View>
 
-                    <View className="mt-4">
-                        <Image source={{ uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGsmGhSaJcQOzDWEwYB31PkUQZTsCsW4YZmQYh6B2c7Q&s' }}
-                            style={[styles.image]} />
-                    </View>
+            <View style={styles.row}>
+              <Text style={styles.familylabel}>{t('education')} : </Text>
+              <Text style={styles.familyDetails}>{parentsData?.education}</Text>
+            </View>
 
-                    <View className="p-[10px]">
-                        <Text className="tracking-wider text-neutral-700 font-semibold text-3xl">Our Approach</Text>
-                        <View className="w-12 h-2 bg-red-400 my-2 rounded"></View>
-                        <Text className="tracking-wider text-neutral-700 font-medium text-base">
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur repellat, iste iusto, culpa recusandae fugiat deserunt molestias praesentium nulla magnam tenetur! Sit neque sapiente tempore, laudantium perferendis eius tenetur dicta.
-                        </Text>
-                        <Text className="tracking-wider text-neutral-700 font-medium text-base mt-8">
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur repellat, iste iusto, culpa recusandae fugiat deserunt molestias praesentium nulla magnam tenetur! Sit neque sapiente tempore, laudantium perferendis eius tenetur dicta.
-                        </Text>
-                    </View>
-
-                    <TouchableOpacity className="flex items-center justify-center bg-rose-400 h-10 w-40 ml-3 mt-4 mb-20  rounded">
-                        <Text className="tracking-wider text-neutral-700 font-medium text-base">Learn More</Text>
-                    </TouchableOpacity>
-                </View>
-            </Animated.ScrollView>
+            <View style={styles.row}>
+              <Text style={styles.familylabel}>{t('profession')} :</Text>
+              <Text style={styles.familyDetails}>{parentsData?.job}</Text>
+            </View>
+          </View>
         </View>
+      </>
     );
+  };
+
+  const renderChildDetails = () => {
+    return childData.length !== 0 ? (
+      childData.map((child, index) => {
+        const childAge = AgeCount(child?.dob);
+        const formattedDate = moment(child?.dob).format('DD/MM/YYYY');
+        return (
+          <View key={index} style={styles.MainContainer}>
+            <View style={styles.familyItem}>
+              <Pressable
+                style={styles.EditIcon}
+                onPress={() =>
+                  navigation.navigate('EditFamilyDetails', {
+                    childId: child && child?._id,
+                  })
+                }>
+                <MaterialCommunityIcons
+                  name="account-edit-outline"
+                  size={30}
+                  color="#00000090"
+                />
+              </Pressable>
+
+
+              <View style={styles.dropdownContent}>
+                <View style={styles.headingRelation}>
+                  <Text style={styles.FamilyName}>
+                    {child && child?.relationship}
+                  </Text>
+                </View>
+              </View>
+
+              <View style={styles.row}>
+                <Text style={styles.familylabel}>{t('name')} : </Text>
+                <Text style={styles.familyDetails}>
+                  {child &&
+                    child?.lastname +
+                    ' ' +
+                    child?.firstname +
+                    ' ' +
+                    child?.middlename}
+                </Text>
+              </View>
+
+              <View style={styles.row}>
+                <Text style={styles.familylabel}>{t('dateofbirth')} : </Text>
+                <Text style={styles.familyDetails}>{formattedDate}</Text>
+              </View>
+
+              <View style={styles.row}>
+                <Text style={styles.familylabel}>{t('age')} : </Text>
+                <Text style={styles.familyDetails}>{childAge} years</Text>
+              </View>
+
+              <View style={styles.row}>
+                <Text style={styles.familylabel}>{t('gender')} :</Text>
+                <Text style={styles.familyDetails}>
+                  {child && child?.gender}
+                </Text>
+              </View>
+
+              <View style={styles.row}>
+                <Text style={styles.familylabel}>{t('education')} : </Text>
+                <Text style={styles.familyDetails}>
+                  {child && child?.education}
+                </Text>
+              </View>
+
+              <View style={styles.row}>
+                <Text style={styles.familylabel}>{t('profession')} :</Text>
+                <Text style={styles.familyDetails}>{child && child?.job}</Text>
+              </View>
+              <View style={styles.DeleteIcon}>
+                <Pressable
+                  // onPress={() => DeleteFamilyMember(child && child?._id)}
+                  onPress={() => setShowModal(true)}>
+                  <MaterialCommunityIcons
+                    name="delete"
+                    size={30}
+                    color="#ff0000"
+                  />
+                </Pressable>
+              </View>
+            </View>
+            {showModal && (
+              <CustomModal
+                showModal={showModal}
+                setShowModal={setShowModal}
+                onConfirm={() => DeleteFamilyMember(child && child?._id)}
+                Title={t('confirm')}
+                Message={t('deleteconfirm')}
+              />
+            )}
+          </View>
+
+        );
+      })
+    ) : (
+      <View style={styles.blankcontainer}>
+        <Text style={styles.blank}>{t('familymembersarenotavailable')}</Text>
+      </View>
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+        {renderParentDetails()}
+        {renderChildDetails()}
+        {childData.length < 7 && (
+          <Pressable
+            style={styles.button}
+            onPress={() =>
+              navigation.navigate('FamilyRegister', {
+                userId: parentsData?._id,
+              })
+            }>
+            <MaterialCommunityIcons name="plus" size={25} color="#fff" />
+            <Text style={styles.btnText}> {t('addnewfamilymembers')}</Text>
+          </Pressable>
+        )}
+
+      </ScrollView>
+    </View>
+  );
 };
 
-export default Aboutus;
-
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-    },
-    image: {
-        width,
-        height: IMG_HEIGHT,
-    },
+  MainContainer: {
+    padding: 15,
+  },
 
+  familyItem: {
+    backgroundColor: '#bbe2ec',
+    padding: 12,
+    borderRadius: 8,
+    shadowColor: '#000000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+    elevation: 4,
+  },
+
+  dropdownContent: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingVertical: 10,
+  },
+
+  EditIcon: {
+    position: 'absolute',
+    right: 7,
+    top: 7,
+  },
+  DeleteIcon: {
+    position: 'absolute',
+    right: 7,
+    bottom: 7,
+  },
+
+  FamilyName: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'black',
+    textTransform: 'capitalize',
+  },
+
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+  },
+
+  familylabel: {
+    alignItems: 'flex-start',
+    flexBasis: '45%',
+    fontSize: 15,
+    color: '#333',
+    fontWeight: '600',
+  },
+
+  familyDetails: {
+    flexBasis: '55%',
+    fontSize: 14,
+    color: '#444',
+    textTransform: 'capitalize',
+  },
+
+  container: {
+    backgroundColor: '#dae4f0',
+    height: '100%',
+    width: '100%',
+    paddingVertical: 20,
+  },
+
+  blankcontainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+
+  blank: {
+    color: 'black',
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+    width: '90%',
+  },
+
+  button: {
+    height: 50,
+    backgroundColor: '#00a9ff',
+    borderRadius: 6,
+    elevation: 5,
+    display: 'flex',
+    flexDirection: 'row',
+    // flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 10,
+    marginHorizontal: 15,
+  },
+
+  btnText: {
+    color: 'white',
+    fontSize: 20,
+    textTransform: 'uppercase',
+    fontWeight: 'bold',
+  },
+
+  // btnText: {
+  //   color: '#fff',
+  //   fontSize: 20,
+  //   textTransform: 'uppercase',
+  //   fontWeight: 'bold',
+  // },
 });
+
+export default FamilyDetailsPage;
---
+++ src/screens/app/Contact/index.jsx
@@ -0,0 +1,57 @@
+import React, { useRef, useState } from 'react';
+import { Animated, Dimensions, StyleSheet, Text, View } from 'react-native';
+
+const { width } = Dimensions.get('screen');
+const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+export default function Contact() {
+    const scrollY = useRef(new Animated.Value(0)).current;
+    const [height1, setHeight1] = useState(100); 
+    const [height2, setHeight2] = useState(0); 
+
+    const translateX = scrollY.interpolate({
+        inputRange: [0, Math.max(height1 - height2, 1)], 
+        outputRange: [-width, 0]
+    });
+
+    return (
+        <View style={styles.container}>
+            <Animated.ScrollView
+                onContentSizeChange={(_, h) => setHeight1(h)}
+                onLayout={e => setHeight2(e.nativeEvent.layout.height)}
+                onScroll={Animated.event(
+                    [{ nativeEvent: { contentOffset: { y: scrollY } } }],
+                    {
+                        useNativeDriver: false,
+                    },
+                )}
+            >
+                {data.map(x => (
+                    <View style={styles.item} key={x}>
+                        <Text>{x}</Text>
+                    </View>
+                ))}
+            </Animated.ScrollView>
+            <Animated.View style={[styles.bar, { transform: [{ translateX }] }]} />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    item: {
+        height: 100,
+        marginVertical: 2,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#bcc2bf'
+    },
+    bar: {
+        height: 10,
+        backgroundColor: '#3d59bf',
+        width: '100%',
+        position: 'absolute'
+    }
+});
